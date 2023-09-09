66b8a7680e00d273a73f72d96357c9e14bce79b1
r-nl <rnelsomain@gmail.com>
2023-09-08 23:30:49 -0600

    Fully build out pages for login, register

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,36 +1,57 @@
 <script lang="ts">
-	import { Block, Button, Card, List, ListInput } from 'konsta/svelte';
-    let username: string;
-    let password: string;
+	import {
+		Block,
+		Button,
+		Card,
+		Link,
+		List,
+		ListInput
+	} from 'konsta/svelte';
+	let email: string;
+	let password: string;
+	let errorMessage: string;
 
-    function login() {
-
-    }
+	function login() {
+		if (!email || !password) {
+			errorMessage = 'Please fill out all forms';
+		} else {
+			errorMessage = '';
+		}
+	}
 </script>
 
 <Block class="flex justify-center">
 	<Card class="w-full sm:w-96">
-        <div class="text-xl">Login</div>
-        <List>
-            <ListInput
-                label="Username"
-                type="text"
-                placeholder="Username"
-                onChange={(changeEvent) => {
-                    username = changeEvent.target.value;
-                }}
-            />
-            <ListInput
-                label="Password"
-                type="password"
-                placeholder="Password"
-                onChange={(changeEvent) => {
-                    password = changeEvent.target.value;
-                }}
-            />
-        </List>
-        <Block>
-            <Button onClick={login}>Login</Button>
-        </Block>
-    </Card>
+		<div class="text-xl px-2">Login</div>
+		<List>
+			<ListInput
+				error={errorMessage}
+				label="Email"
+				type="text"
+				placeholder="Email"
+				onChange={(changeEvent) => {
+					email = changeEvent.target.value;
+				}}
+			/>
+			<ListInput
+				error={errorMessage}
+				label="Password"
+				type="password"
+				placeholder="Password"
+				onChange={(changeEvent) => {
+					password = changeEvent.target.value;
+				}}
+			/>
+		</List>
+		<Block>
+			<Button onClick={login}>Login</Button>
+		</Block>
+		<Block class="flex justify-center">
+			<div>
+				Dont have an account? <Link linkProps={{ href: '/register' }}>
+					Register
+				</Link>
+			</div>
+		</Block>
+	</Card>
 </Block>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,44 +1,66 @@
 <script lang="ts">
-	import { Block, Button, Card, List, ListInput } from 'konsta/svelte';
-    let newUsername: string;
-    let newPassword: string;
+	import {
+		Block,
+		Button,
+		Card,
+		Link,
+		List,
+		ListInput
+	} from 'konsta/svelte';
+	let newEmail: string;
+	let newPassword: string;
+	let confirmationPassword: string;
+	let errorMessage: string;
 
-    function registerNewUser() {
-
-    }
+	function registerNewUser() {
+		if (!newEmail || !newPassword || !confirmationPassword) {
+			errorMessage = 'Please fill out all forms';
+		} else {
+			errorMessage = '';
+		}
+	}
 </script>
 
 <Block class="flex justify-center">
 	<Card class="w-full sm:w-96">
-        <div class="text-xl">Register</div>
-        <List>
-            <ListInput
-                label="Username"
-                type="text"
-                placeholder="Username"
-                onChange={(changeEvent) => {
-                    newUsername = changeEvent.target.value;
-                }}
-            />
-            <ListInput
-                label="Password"
-                type="password"
-                placeholder="Password"
-                onChange={(changeEvent) => {
-                    newPassword = changeEvent.target.value;
-                }}
-            />
-            <ListInput
-                label="Confirm Password"
-                type="password"
-                placeholder="Retype Password"
-                onChange={(changeEvent) => {
-                    newPassword = changeEvent.target.value;
-                }}
-            />
-        </List>
-        <Block>
-            <Button onClick={registerNewUser}>Register</Button>
-        </Block>
-    </Card>
+		<div class="text-xl px-2">Register</div>
+		<List>
+			<ListInput
+				error={errorMessage}
+				label="Email"
+				type="text"
+				placeholder="Email"
+				onChange={(changeEvent) => {
+					newEmail = changeEvent.target.value;
+				}}
+			/>
+			<ListInput
+				error={errorMessage}
+				label="Password"
+				type="password"
+				placeholder="Password"
+				onChange={(changeEvent) => {
+					newPassword = changeEvent.target.value;
+				}}
+			/>
+			<ListInput
+				label="Confirm Password"
+				type="password"
+				placeholder="Retype Password"
+				onChange={(changeEvent) => {
+					confirmationPassword = changeEvent.target.value;
+				}}
+			/>
+		</List>
+		<Block>
+			<Button onClick={registerNewUser}>Register</Button>
+		</Block>
+		<Block class="flex justify-center">
+			<div>
+				Already have an account? <Link linkProps={{ href: '/login' }}>
+					Login
+				</Link>
+			</div>
+		</Block>
+	</Card>
 </Block>

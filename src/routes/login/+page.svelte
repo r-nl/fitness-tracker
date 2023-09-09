c8265ba249e2cb0af948f40e2acb1f1209e0c914
r-nl <rnelsomain@gmail.com>
2023-09-08 23:16:32 -0600

    Buildout Register and login pages

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,7 +1,36 @@
 <script lang="ts">
-	import { Block } from 'konsta/svelte';
+	import { Block, Button, Card, List, ListInput } from 'konsta/svelte';
+    let username: string;
+    let password: string;
+
+    function login() {
+
+    }
 </script>
 
-<Block>
-	<h1>Login</h1>
+<Block class="flex justify-center">
+	<Card class="w-full sm:w-96">
+        <div class="text-xl">Login</div>
+        <List>
+            <ListInput
+                label="Username"
+                type="text"
+                placeholder="Username"
+                onChange={(changeEvent) => {
+                    username = changeEvent.target.value;
+                }}
+            />
+            <ListInput
+                label="Password"
+                type="password"
+                placeholder="Password"
+                onChange={(changeEvent) => {
+                    password = changeEvent.target.value;
+                }}
+            />
+        </List>
+        <Block>
+            <Button onClick={login}>Login</Button>
+        </Block>
+    </Card>
 </Block>
--- src/routes/register/+page.svelte
+++ src/routes/register/+page.svelte
@@ -1,7 +1,44 @@
 <script lang="ts">
-	import { Block } from 'konsta/svelte';
+	import { Block, Button, Card, List, ListInput } from 'konsta/svelte';
+    let newUsername: string;
+    let newPassword: string;
+
+    function registerNewUser() {
+
+    }
 </script>
 
-<Block>
-	<h1>Register</h1>
+<Block class="flex justify-center">
+	<Card class="w-full sm:w-96">
+        <div class="text-xl">Register</div>
+        <List>
+            <ListInput
+                label="Username"
+                type="text"
+                placeholder="Username"
+                onChange={(changeEvent) => {
+                    newUsername = changeEvent.target.value;
+                }}
+            />
+            <ListInput
+                label="Password"
+                type="password"
+                placeholder="Password"
+                onChange={(changeEvent) => {
+                    newPassword = changeEvent.target.value;
+                }}
+            />
+            <ListInput
+                label="Confirm Password"
+                type="password"
+                placeholder="Retype Password"
+                onChange={(changeEvent) => {
+                    newPassword = changeEvent.target.value;
+                }}
+            />
+        </List>
+        <Block>
+            <Button onClick={registerNewUser}>Register</Button>
+        </Block>
+    </Card>
 </Block>
